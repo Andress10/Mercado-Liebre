939a2e8e59d2c29ceb427d108bf611b7b9b3047d
Andress10 <andres_20nuz@hotmail.com>
2021-11-16 19:41:58 -0300

    Subiendo Formularios

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express') //Tequerimos express
+const express = require('express') //Requerimos express
 const app = express() 
 app.use(express.static('public'))
 
+const path = require('path')
 
-app.listen(3000,()=>{
+app.listen(3020,()=>{
     console.log('Servidor funcionando')
 })
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + ('/views/home.html'))
 })
+
+app.get('/register',(req,res) => {
+    res.sendFile(__dirname + ('/views/register.html'))
+});
+
+app.get('/login',(req,res) => {
+    res.sendFile(__dirname + ('/views/login.html'))
+});
 
 
