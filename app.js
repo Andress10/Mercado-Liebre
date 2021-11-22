39c8923f1db3d57e3ab7af0cb637c500ef855385
Andress10 <andres_20nuz@hotmail.com>
2021-11-21 21:56:02 -0300

    probando si anda heroku 1

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-const express = require('express') //Requerimos express
-const app = express() 
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
 
-const path = require('path')
+app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3020,()=>{
-    console.log('Servidor funcionando')
-})
-// app.listen(3020,()=>{
-//     console.log('Servidor funcionando')
-// })
+app.listen(process.env.PORT || 3020, () => console.log(`Servidor funcionando`));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + ('/views/home.html'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
 })
 
-app.get('/register',(req,res) => {
-    res.sendFile(__dirname + ('/views/register.html'))
-});
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+})
 
-app.get('/login',(req,res) => {
-    res.sendFile(__dirname + ('/views/login.html'))
-});
-
-
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+})
