76a8ed40829c841a4d33ae0bc5d74e338509d6a3
Andress10 <andres_20nuz@hotmail.com>
2021-11-21 21:06:57 -0300

    subir a heroku

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ app.use(express.static('public'))
 
 const path = require('path')
 
-app.listen(3020,()=>{
+app.listen(process.env.PORT ||3020,()=>{
     console.log('Servidor funcionando')
 })
+// app.listen(3020,()=>{
+//     console.log('Servidor funcionando')
+// })
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + ('/views/home.html'))
